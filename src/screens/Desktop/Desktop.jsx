370360bb7be865d963276f098977468c3d5c0eac
Durgesh Kumar Pandey <98370352+pandeydurgeshxyz@users.noreply.github.com>
2023-10-19 09:43:03 +0530

    Adding initial files

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,341 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <div className="group">
+            <div className="group-2">
+              <div className="group-3">
+                <div className="text-wrapper">Display</div>
+                <img className="vector" alt="Vector" src="https://c.animaapp.com/sGkJ5FUn/img/vector.svg" />
+              </div>
+              <img className="img" alt="Group" src="https://c.animaapp.com/sGkJ5FUn/img/group-41@2x.png" />
+            </div>
+          </div>
+        </div>
+        <div className="group-wrapper">
+          <div className="group-4">
+            <div className="group-5">
+              <div className="group-6">
+                <div className="overlap-wrapper">
+                  <div className="overlap-group">
+                    <div className="text-wrapper-2">Add Multi-Language Support</div>
+                    <div className="group-7">
+                      <div className="text-wrapper-3">CAM-4</div>
+                      <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                          <img
+                            className="ellipse"
+                            alt="Ellipse"
+                            src="https://c.animaapp.com/sGkJ5FUn/img/ellipse-2-9@2x.png"
+                          />
+                          <div className="ellipse-2" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-4">---</div>
+                    </div>
+                    <div className="overlap-2">
+                      <div className="ellipse-3" />
+                      <div className="text-wrapper-5">Feature Request</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="group-8">
+                  <div className="overlap-group">
+                    <div className="text-wrapper-2">Implement Email Notification System</div>
+                    <div className="group-7">
+                      <div className="text-wrapper-3">CAM-4</div>
+                      <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                          <img
+                            className="ellipse"
+                            alt="Ellipse"
+                            src="https://c.animaapp.com/sGkJ5FUn/img/ellipse-2-8@2x.png"
+                          />
+                          <div className="ellipse-2" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-4">---</div>
+                    </div>
+                    <div className="overlap-2">
+                      <div className="ellipse-3" />
+                      <div className="text-wrapper-5">Feature Request</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="group-9">
+                  <div className="overlap-group">
+                    <p className="text-wrapper-2">Update User Profile Page UI</p>
+                    <div className="group-7">
+                      <div className="text-wrapper-3">CAM-1</div>
+                      <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                          <img
+                            className="ellipse"
+                            alt="Ellipse"
+                            src="https://c.animaapp.com/sGkJ5FUn/img/ellipse-2-7@2x.png"
+                          />
+                          <div className="ellipse-2" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-4">---</div>
+                    </div>
+                    <div className="overlap-2">
+                      <div className="ellipse-3" />
+                      <div className="text-wrapper-5">Feature Request</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="group-10">
+                <div className="text-wrapper-6">To do</div>
+                <div className="text-wrapper-7">3</div>
+                <img className="group-11" alt="Group" src="https://c.animaapp.com/sGkJ5FUn/img/group-23-2@2x.png" />
+                <img className="group-12" alt="Group" src="https://c.animaapp.com/sGkJ5FUn/img/group-27-4@2x.png" />
+              </div>
+            </div>
+            <div className="group-13">
+              <div className="group-14">
+                <div className="group-15">
+                  <div className="overlap-3">
+                    <p className="add-multi-language">
+                      Add Multi-Language Support-Enable
+                      <br />
+                      multi-language support within the...
+                    </p>
+                    <div className="group-7">
+                      <div className="text-wrapper-3">CAM-5</div>
+                      <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                          <img
+                            className="ellipse"
+                            alt="Ellipse"
+                            src="https://c.animaapp.com/sGkJ5FUn/img/ellipse-2-6@2x.png"
+                          />
+                          <div className="ellipse-2" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="overlap-4">
+                      <div className="text-wrapper-4">---</div>
+                    </div>
+                    <div className="overlap-5">
+                      <div className="ellipse-3" />
+                      <div className="text-wrapper-5">Feature Request</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="group-16">
+                  <div className="overlap-group">
+                    <p className="text-wrapper-2">Create Onboarding Tutorial for New Users</p>
+                    <div className="group-7">
+                      <div className="text-wrapper-3">CAM-8</div>
+                      <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                          <img
+                            className="ellipse"
+                            alt="Ellipse"
+                            src="https://c.animaapp.com/sGkJ5FUn/img/ellipse-2-5@2x.png"
+                          />
+                          <div className="ellipse-2" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-4">---</div>
+                    </div>
+                    <div className="overlap-2">
+                      <div className="ellipse-3" />
+                      <div className="text-wrapper-5">Feature Request</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="group-10">
+                <div className="text-wrapper-6">To do</div>
+                <div className="text-wrapper-7">3</div>
+                <img className="group-11" alt="Group" src="https://c.animaapp.com/sGkJ5FUn/img/group-23-2@2x.png" />
+                <img className="group-12" alt="Group" src="https://c.animaapp.com/sGkJ5FUn/img/group-27-4@2x.png" />
+              </div>
+            </div>
+            <div className="group-17">
+              <div className="group-18">
+                <div className="group-19">
+                  <div className="overlap-group">
+                    <p className="text-wrapper-2">Optimize Databse Queries for Performance</p>
+                    <div className="group-7">
+                      <div className="text-wrapper-3">CAM-3</div>
+                      <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                          <img
+                            className="ellipse"
+                            alt="Ellipse"
+                            src="https://c.animaapp.com/sGkJ5FUn/img/ellipse-2-4@2x.png"
+                          />
+                          <div className="ellipse-2" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-4">---</div>
+                    </div>
+                    <div className="overlap-2">
+                      <div className="ellipse-3" />
+                      <div className="text-wrapper-5">Feature Request</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="group-10">
+                <div className="text-wrapper-6">In Progress</div>
+                <div className="text-wrapper-8">1</div>
+                <img className="group-11" alt="Group" src="https://c.animaapp.com/sGkJ5FUn/img/group-23@2x.png" />
+                <img className="group-12" alt="Group" src="https://c.animaapp.com/sGkJ5FUn/img/group-27-4@2x.png" />
+              </div>
+            </div>
+            <div className="group-20">
+              <div className="group-21">
+                <div className="overlap-wrapper">
+                  <div className="overlap-group">
+                    <div className="text-wrapper-2">Enhance Search Functionally</div>
+                    <div className="group-7">
+                      <div className="text-wrapper-3">CAM-6</div>
+                      <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                          <img
+                            className="ellipse"
+                            alt="Ellipse"
+                            src="https://c.animaapp.com/sGkJ5FUn/img/ellipse-2-3@2x.png"
+                          />
+                          <div className="ellipse-2" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-4">---</div>
+                    </div>
+                    <div className="overlap-2">
+                      <div className="ellipse-3" />
+                      <div className="text-wrapper-5">Feature Request</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="group-8">
+                  <div className="overlap-group">
+                    <div className="text-wrapper-2">Integrate Third -Party GateWay</div>
+                    <div className="group-7">
+                      <div className="text-wrapper-3">CAM-7</div>
+                      <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                          <img
+                            className="ellipse"
+                            alt="Ellipse"
+                            src="https://c.animaapp.com/sGkJ5FUn/img/ellipse-2-2@2x.png"
+                          />
+                          <div className="ellipse-2" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-4">---</div>
+                    </div>
+                    <div className="overlap-2">
+                      <div className="ellipse-3" />
+                      <div className="text-wrapper-5">Feature Request</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="group-22">
+                  <div className="overlap-6">
+                    <div className="text-wrapper-2">
+                      Conduct Security Vulnerability
+                      <br />
+                      Assessment
+                    </div>
+                    <div className="group-7">
+                      <div className="text-wrapper-3">CAM-4</div>
+                      <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                          <img
+                            className="ellipse"
+                            alt="Ellipse"
+                            src="https://c.animaapp.com/sGkJ5FUn/img/ellipse-2-1@2x.png"
+                          />
+                          <div className="ellipse-2" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="overlap-7">
+                      <div className="text-wrapper-4">---</div>
+                    </div>
+                    <div className="overlap-8">
+                      <div className="ellipse-3" />
+                      <div className="text-wrapper-5">Feature Request</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="group-23">
+                  <div className="overlap-group">
+                    <div className="text-wrapper-2">Upgrade Server Interface</div>
+                    <div className="group-7">
+                      <div className="text-wrapper-3">CAM-4</div>
+                      <div className="overlap-group-wrapper">
+                        <div className="overlap-group-2">
+                          <img
+                            className="ellipse"
+                            alt="Ellipse"
+                            src="https://c.animaapp.com/sGkJ5FUn/img/ellipse-2@2x.png"
+                          />
+                          <div className="ellipse-2" />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="div-wrapper">
+                      <div className="text-wrapper-4">---</div>
+                    </div>
+                    <div className="overlap-2">
+                      <div className="ellipse-3" />
+                      <div className="text-wrapper-5">Feature Request</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="group-24">
+                <div className="group-25">
+                  <div className="text-wrapper-9">Done</div>
+                  <div className="text-wrapper-10">5</div>
+                  <img className="group-26" alt="Group" src="https://c.animaapp.com/sGkJ5FUn/img/group-27-4@2x.png" />
+                </div>
+                <div className="vector-wrapper">
+                  <img className="vector-2" alt="Vector" src="https://c.animaapp.com/sGkJ5FUn/img/vector-3.svg" />
+                </div>
+              </div>
+            </div>
+            <div className="group-27">
+              <div className="overlap-9">
+                <div className="group-28">
+                  <div className="group-29">
+                    <div className="text-wrapper-9">Cancle</div>
+                    <img className="group-30" alt="Group" src="https://c.animaapp.com/sGkJ5FUn/img/group-27-4@2x.png" />
+                  </div>
+                  <div className="group-31" />
+                </div>
+                <img className="vector-3" alt="Vector" src="https://c.animaapp.com/sGkJ5FUn/img/vector-2.svg" />
+                <img className="vector-3" alt="Vector" src="https://c.animaapp.com/sGkJ5FUn/img/vector-1.svg" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
